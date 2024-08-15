13db0cb8eccd62caf2582447cdd0eb2f72fd5d79
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-08-15 17:44:31 -0400

    updated wooo

--- src/views/Currently.jsx
+++ src/views/Currently.jsx
@@ -7,17 +7,15 @@ function Currently() {
     <h1>What am I currently working on?</h1>
     
       <div className="block">
-        <h2>SEO Tech Developer Program</h2>
-        <p>I am currently participating in the SEO Tech Developer Program</p>
-        <p>
-          The program provides comprehensive training and mentoring in a variety of topics. We cover a variety of topics, including APIs, JSON, Databases, SQL, Unit Testing, Development Pipelines, Cyber Security, Data Engineering, Front End Technologies, Web App Security, UI/UX Design, Agile and Scrum, DevOps, and Cloud Engineering.
-        </p>
-        <p><i>Last update: June 26th, 2024</i></p>
+        <h2> Data Structures and Algorithms </h2>
+        <p> I am currently practicing and improving my skills in data structures and algorithms. I focus on solving a variety of problems to strengthen my understanding and enhance my problem-solving abilities. </p>
+        <p><i>Last update: August 15th, 2024</i></p>
       </div>
       
       <div className="block">
         <h2>Code Review and Improvement</h2>
         <p>I am currently going through my old code to try to make it more readable and user-friendly.</p>
+        <p><i>Last update: August 1st, 2024</i></p>
       </div>
   </div>
 
