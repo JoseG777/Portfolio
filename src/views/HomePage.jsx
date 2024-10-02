24e56ded77d28110ba21ced8702912fff70f0d92
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-10-01 22:19:38 -0400

    updated portfolio

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -7,13 +7,13 @@ function HomePage() {
       <h1>Welcome to My Portfolio!</h1>
       <h2>Hi, I'm Jose Guzman</h2>
       <p>
-        I’m a junior at Hunter College, majoring in Computer Science. Honestly, I kind of just chose CS on a whim (and because of the money), but it ended up being something I really enjoy. The more I learned, the more I realized how much I can actually do with code. I love the challenge of figuring things out and the feeling of finally solving a problem after banging my head against it for a while.
+        I’m a junior at Hunter College, majoring in Computer Science. Honestly, I kind of just chose CS on a whim (and because of the money), but it ended up being something I really enjoy. The more I learn, the more I realized how much I can actually do with code. I love the challenge of figuring things out and the feeling of finally solving a problem after banging my head against it for a while.
       </p>
       <p>
-        I’m always working on personal projects to push myself and learn new things. I’ve had my fair share of failures along the way, but I try to see them as part of the process—each one teaches me something new and helps me get better.
+        I’m always working on personal projects to push myself and learn new things/ improve on the skills I currently have. I’ve had my fair share of failures along the way, but I try to see them as part of the process as each one teaches me something new and helps me get better.
       </p>
       <p>
-        When I’m not coding, I love to swim and read. Right now, I’m reading <em>Pedro Páramo</em> by Juan Rulfo—mostly to brush up on my Spanish, but it’s also a pretty great story.
+        When I’m not coding, I love to swim and read. Right now, I’m reading <em>Pedro Páramo</em> by Juan Rulfo—mostly to brush up on my Spanish.
       </p>
     </div>
   );
