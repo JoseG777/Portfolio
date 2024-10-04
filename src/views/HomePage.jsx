7757ab0c18f746e94f31520b2cf2f4a00332eaf1
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-10-04 19:06:10 -0400

    small update

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -7,7 +7,7 @@ function HomePage() {
       <h1>Welcome to My Portfolio!</h1>
       <h2>Hi, I'm Jose Guzman</h2>
       <p>
-        I’m a junior at Hunter College, majoring in Computer Science. Honestly, I kind of just chose CS on a whim (and because of the money), but it ended up being something I really enjoy. The more I learn, the more I realized how much I can actually do with code. I love the challenge of figuring things out and the feeling of finally solving a problem after banging my head against it for a while.
+        I’m a junior at Hunter College, majoring in Computer Science. I didn't have any tech experience coming into college so I kind of just chose CS on a whim, but it ended up being something I really enjoy. The more I learn, the more I realized how much I can actually do with code. I love the challenge of figuring things out and the feeling of finally solving a problem after banging my head against it for a while.
       </p>
       <p>
         I’m always working on personal projects to push myself and learn new things/ improve on the skills I currently have. I’ve had my fair share of failures along the way, but I try to see them as part of the process as each one teaches me something new and helps me get better.
